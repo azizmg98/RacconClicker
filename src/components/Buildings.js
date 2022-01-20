831a3b8f134474a2e608b72515ab9ab0200150d1
aziz <azizalghanim98@gmail.com>
2022-01-20 19:20:02 +0300

    basic buildings

---
+++ src/components/Buildings.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const Buildings = () => {
+    // const [building, setBuilding] = useState(1);
+    // const incrementBuilding = () => setBuilding(building + 1);  
+    return <div>
+      <button onClick={}></button>
+  </div>;
+};
+
+export default Buildings;
--- src/components/Points.js
+++ src/components/Points.js
@@ -2,12 +2,17 @@ import React, { useState } from 'react';
 import image from '../images/raccoonClick.jpg'
 
 const Points = () => {
-  const [food, setFood] = useState(0)
-  const incrementFood = () =>setFood(food + 1);
+  const [building, setBuilding] = useState(1);
+  const incrementBuilding = () => setBuilding(building + 1);
+  const [food, setFood] = useState(0);
+  const incrementFood = () => setFood(food + building);
+  // const foodPerSecond = () => setInterval(incrementFood, 1000);
   return <div>
     <div className='App'>
     <img onClick={incrementFood} className='clicker'  src={image} alt='fat raccoon'/>
-    <button onClick={() => alert("you got baited")}>Click Me</button>
+    {/* <button onClick={() => alert("you got baited")}>Click Me</button> */}
+    <button onClick={incrementBuilding}>building placeholder</button>
+    <button onClick={setInterval(incrementFood, 1000)}>building per second</button>
     <h1>{food}</h1>
     </div>
   </div>;
